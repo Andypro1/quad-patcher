6216f589c8226339f5a02060341fc06c1d9856c1
Andypro1 <871829+Andypro1@users.noreply.github.com>
2025-07-20 17:51:00 -0400

    Add a patch select form element and associated styles.
    Make css transitions use a :root var and be fast (0.1s).
    
    Add new #f537 NES fixes/tweaks patch for public beta testing, yay!
    
    Update page.svelte to reinitialize RomPatcherJS when the selected patch changes.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,30 @@
 <script lang="js">
-    import { onMount } from "svelte";
-    let error = "";
+    import { page } from '$app/state';
+    import { onMount } from 'svelte';
+    import { get } from 'svelte/store';
 
+    let error = "";
+    
+    let patches = $state([
+        {
+            id: 'f537',
+            file: './f537.ips',
+            nameAddendum: 'nes-fixes-f537',
+            name: 'NES fixes/tweaks #f537',
+            description: `A patch implementing a number of NES game updates.  Asking for testers before submitting to Total for inclusion in the official builds.  I\'m requesting you play this <a href="https://en.wikipedia.org/wiki/Blinded_experiment">blind</a> (no release notes) to help me best guage what to work on next.
+            <p>Feedback encouraged <a href="https://discord.com/channels/567470487483973652/638112063780159499">on Discord</a>.  Thank you for testing!</p>`,
+        },
+        {
+            id: 'xray',
+            file: './xray.ips',
+            nameAddendum: 'xray-mirage',
+            name: 'X-ray Mirage Mode',
+            description: 'A challenge mod for your quad seed.  Link and Samus are suffering hallucinations from dehydration, and most items look like the much-maligned x-ray scope.',
+        },
+    ]);
+
+    let chosenPatch = $state("");
+    
     const expectedSeedSize = 8388608;
 
     function formatBytes(bytes, decimals = 2) {
@@ -27,6 +50,20 @@
     }
 
     onMount(() => {
+        const query = page.url.searchParams;
+        const qsValue = query.get('p');
+
+        if (qsValue && patches.filter(p => p.id == qsValue)?.length > 0) {
+            chosenPatch = qsValue;
+        }
+    });
+
+    $effect(() => {
+        const curPatch = patches.find(p => p.id === chosenPatch);
+
+        if(!curPatch)
+            return;
+
         try {
             RomPatcherWeb.initialize({
                 language: 'en', //default: en
@@ -48,13 +85,13 @@
                         lastDotIndex !== -1
                             ? romFile.fileName.substring(0, lastDotIndex)
                             : romFile.fileName;
-                    const outFileName = `${baseName}.appatch-xray-mirage`;
+                    const outFileName = `${baseName}.appatch-${curPatch.nameAddendum}`;
                     romFile.setName(outFileName);
                 }
             }, {
-                file: './xray.ips',
-                name: 'X-ray Mirage Mode',
-                description: 'A challenge mod for your quad seed.  Link and Samus are suffering hallucinations from dehydration, and most items look like the much-maligned x-ray scope.',
+                file: curPatch.file,
+                name: curPatch.name,
+                description: curPatch.description,
             });
         } catch (err) {
             var message = err.message;
@@ -76,7 +113,23 @@
 <main on:dragover|preventDefault>
     <h1>Quad Randomizer quick patcher</h1>
 
-	<div id="rom-patcher-container">
+    <form>
+        <div class="patchChooser">
+            <label for="patch">Select a patch:</label>
+
+            {#snippet content()}
+                <button><selectedcontent>Quad rando patch</selectedcontent></button>
+            {/snippet}
+
+            <select id="patch" bind:value={chosenPatch}>
+                {@render content()}
+                {#each patches as patch}
+                    <option value={patch.id}>{@html patch.name}</option>
+                {/each}
+            </select>
+        </div>
+
+	<div id="rom-patcher-container" class:visible={chosenPatch}>
         {#if error}
             <p style="color: red;">{error}</p>
         {/if}
@@ -106,17 +159,128 @@
 			<button id="rom-patcher-button-apply" data-localize="yes" disabled>Apply patch</button>
 		</div>
 	</div>
+
+    </form>
 </main>
 
 <style>
+    :global(:root) {
+        --transition-time: 0.1s;
+    }
+
+    /*  Select control styles  */
+    select, ::picker(select) {
+        appearance: base-select;
+        
+    }
+
+    select {
+        cursor: pointer;
+        border: 2px solid #ddd;
+        background: #eee;
+        padding: 10px;
+        transition: var(--transition-time);
+        /* border-radius: 0.25rem 0.25rem 0 0; */
+
+        min-width: 20rem;
+
+        & option {
+            display: flex;
+            justify-content: flex-start;
+            gap: 20px;
+
+            border: 2px solid #ddd;
+            background: #eee;
+            padding: 10px;
+            transition: var(--transition-time);
+        }
+    }
+
+    option:first-of-type {
+    border-radius: 8px 8px 0 0;
+    }
+
+    option:last-of-type {
+    border-radius: 0 0 8px 8px;
+    }
+
+    option:not(option:last-of-type) {
+    border-bottom: none;
+    }
+
+option .icon {
+  font-size: 1.6rem;
+  text-box: trim-both cap alphabetic;
+}
+
+option:hover {
+  background: rgb(0, 123, 255);
+  color: white;
+}
+
+option:checked {
+  font-weight: bold;
+}
+
+option::checkmark {
+  order: 1;
+  margin-left: auto;
+  content: "✔️";
+  color: transparent;
+  text-shadow: 0 0 0 rgb(0, 123, 255);
+}
+
+    ::picker(select) {
+      border: none;
+    }
+
+    select:hover,
+    select:focus {
+        background: #ddd;
+    }
+
+    select::picker-icon {
+        color: #999;
+        transition: var(--transition-time) rotate;
+    }
+
+    select:open::picker-icon {
+        rotate: 180deg;
+    }
+
+    select > .placeholder {
+        color: #ddd;
+    }
+    /*  End select styles  */
+
+    .patchChooser {
+        display: flex;
+        margin: 2rem 0;
+        gap: 1rem;
+        justify-content: center;
+        align-items: center;
+    }
+
     .test {
         background-color: rgb(0, 123, 255);
         color: white;
         padding: 1rem 3rem;
     }
 
+    #rom-patcher-container {
+        display: none;
+
+        &.visible {
+            display: block;
+        }
+    }
+
     #rom-patcher-patch-description {
         text-align: left;
+    }
+
+    select > .placeholder {
+        display: none;
     }
 
     h1 {
@@ -153,14 +317,14 @@
         cursor: pointer;
         width: 100%;
         max-width: 400px;
-        transition: border-color 0.3s;
+        transition: border-color var(--transition-time);
     }
 
-    input[type="file"].error {
+    #rom-patcher-container input[type="file"].error {
         border-color: red;
     }
 
-    button {
+    #rom-patcher-container button {
         padding: 6px 2rem;
         font-size: 1em;
         font-weight: normal;
@@ -169,14 +333,14 @@
         background-color: rgb(40, 167, 69);
         color: white;
         cursor: pointer;
-        transition: background-color 0.3s;
+        transition: background-color var(--transition-time);
 
         &:hover {
             background-color: rgb(33, 136, 56);
         }
     }
 
-    button:disabled {
+    #rom-patcher-container button:disabled {
         background-color: rgb(223, 223, 223);
         border-color: rgb(223, 223, 223);
         cursor: not-allowed;
