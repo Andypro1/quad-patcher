0f827af6e91d3304557d6943aff3eafedf6ea2f7
Andypro1 <871829+Andypro1@users.noreply.github.com>
2025-07-29 15:48:11 -0400

    Updating nes patch to #6484.  Fixes beta seed regression found by hatch78.

--- src/routes/[[patch]]/+page.svelte
+++ src/routes/[[patch]]/+page.svelte
@@ -7,10 +7,10 @@
     
     let patches = $state([
         {
-            id: '409c',
-            file: './409c.ips',
-            nameAddendum: 'nes-fixes-409c',
-            name: 'NES fixes/tweaks #409c',
+            id: '6484',
+            file: './6484.ips',
+            nameAddendum: 'nes-fixes-6484',
+            name: 'NES fixes/tweaks #6484',
             description: `A patch implementing a number of NES game updates.  Asking for testers before submitting to Total for inclusion in the official builds.  I\'m requesting you play this <a href="https://en.wikipedia.org/wiki/Blinded_experiment">blind</a> (no release notes) to help me best guage what to work on next.
             <p>There are <strong>14</strong> changes in this patch.  How many can you find?  Feedback encouraged <a href="https://discord.com/channels/567470487483973652/638112063780159499">on Discord</a>.  Thank you for testing!</p>`,
         },
